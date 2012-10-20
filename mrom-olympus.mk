efe3003ae3c9d3122aa9823b4a976d35e75cef25
Michael Montuori <michael.montuori@gmail.com>
2012-10-20 00:57:06 -0400

    MROM vendor files

---
+++ mrom-olympus.mk
@@ -0,0 +1,9 @@
+# Generic cyanogenmod product
+PRODUCT_NAME := mrom
+PRODUCT_BRAND := mrom
+PRODUCT_DEVICE := generic
+
+PRODUCT_COPY_FILES += \
+	vendor/mrom/prebuilt/common/system/lib/libnmea.so.234:system/lib/libnmea.so.234 \
+	vendor/mrom/prebuilt/common/system/bin/gpsfix234:system/bin/gpsfix234 \
+	vendor/mrom/prebuilt/common/system/app/BatteryFix.apk:system/app/BatteryFix.apk
---
+++ mrom.mk
@@ -0,0 +1,18 @@
+# Generic cyanogenmod product
+PRODUCT_NAME := mrom
+PRODUCT_BRAND := mrom
+PRODUCT_DEVICE := generic
+
+PRODUCT_COPY_FILES += \
+	vendor/mrom/prebuilt/common/system/etc/init.d/90mrom:system/etc/init.d/90mrom \
+	vendor/mrom/prebuilt/common/system/lib/libqpicjni96.so:system/lib/libqpicjni96.so \
+	vendor/mrom/prebuilt/common/system/bin/fullgpu:system/bin/fullgpu \
+	vendor/mrom/prebuilt/common/system/xbin/su:system/xbin/su \
+	vendor/mrom/prebuilt/common/system/app/QuickPic.apk:system/app/QuickPic.apk \
+	vendor/mrom/prebuilt/common/system/app/MIUIcamera.apk:system/app/MIUIcamera.apk \
+	vendor/mrom/prebuilt/common/system/app/Mixer-MROM.apk:system/app/Mixer-MROM.apk \
+	vendor/mrom/prebuilt/common/system/app/Supersu.apk:system/app/Supersu.apk \
+	vendor/mrom/prebuilt/common/system/app/HoloLauncher.apk:system/app/HoloLauncher.apk \
+	vendor/mrom/prebuilt/common/system/app/MROM.apk:system/app/MROM.apk
+
+$(call inherit-product, vendor/mrom/mrom-olympus.mk)
