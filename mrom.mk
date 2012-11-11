8b6e2b9c0c572ff0a6bc3c3db315994b9c720891
Michael Montuori <michael.montuori@gmail.com>
2012-11-11 00:20:33 -0500

    changed package dependencies

--- mrom.mk
+++ mrom.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
 	vendor/mrom/prebuilt/common/system/app/Supersu.apk:system/app/Supersu.apk \
 	vendor/mrom/CHANGELOG-MROM.txt:system/etc/CHANGELOG-MROM.txt
 
-$(call inherit-product, vendor/mrom/mrom-olympus.mk)
+$(call inherit-product, vendor/mrom/mrom-$(CM_BUILD).mk)
 
 PRODUCT_PACKAGES += \
     MROM
