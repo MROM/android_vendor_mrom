77ad98085dc33d560ee187a487c442996d9bdbc4
mmontuori <michael.montuori@gmail.com>
2013-02-05 22:36:30 -0500

    Added Mixer JB Theme

--- mrom.mk
+++ mrom.mk
@@ -5,7 +5,7 @@ PRODUCT_COPY_FILES += \
 	vendor/mrom/prebuilt/common/system/lib/libqpicjni100.so:system/lib/libqpicjni100.so \
 	vendor/mrom/prebuilt/common/system/xbin/su:system/xbin/su \
 	vendor/mrom/prebuilt/common/system/app/QuickPic.apk:system/app/QuickPic.apk \
-	vendor/mrom/prebuilt/common/system/app/Mixer-CM9.apk:system/app/Mixer-CM9.apk \
+	vendor/mrom/prebuilt/common/system/app/Mixer-JB.apk:system/app/Mixer-JB.apk \
 	vendor/mrom/prebuilt/common/system/app/Supersu.apk:system/app/Supersu.apk \
 	vendor/mrom/CHANGELOG-MROM.txt:system/etc/CHANGELOG-MROM.txt
 
