7dd0edd7c988c89ef4a436d246945983c286318a
mmontuori <michael.montuori@gmail.com>
2013-02-16 00:37:04 -0500

    TheUnkn0wn camera for SGS3

--- mrom-d2att.mk
+++ mrom-d2att.mk
@@ -1,3 +1,7 @@
 # Generic cyanogenmod product
 PRODUCT_DEVICE := d2att
 
+PRODUCT_COPY_FILES += \
+        vendor/mrom/prebuilt/common/system/bon/Gallery2.apk:system/bin/Gallery2.apk \
+        vendor/mrom/prebuilt/common/system/etc/media_profiles.xml:system/etc/media_profiles.xml 
+
