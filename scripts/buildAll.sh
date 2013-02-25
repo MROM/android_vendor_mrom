7a48e5b365c911e346fa508e91c3e98effe7bb1c
mmontuori <michael.montuori@gmail.com>
2013-02-25 18:57:11 -0500

    build add script

---
+++ scripts/buildAll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test -d out; then
+	echo "ERROR: out sidecroty found!!!"
+	exit
+fi
+
+devices="d2att d2vzw"
+
+for device in $devices; do
+	echo "building $device..."
+	mv out.$device out
+	./makeMROM.sh $device	
+	mv out out.$device
+done
