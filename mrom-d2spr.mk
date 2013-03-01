5f5021a9be8dd0b04b44fdfec959eecf92e386de
mmontuori <michael.montuori@gmail.com>
2013-03-01 01:05:02 -0500

    d2spr and d2tmo

---
+++ mrom-d2spr.mk
@@ -0,0 +1,7 @@
+# Generic cyanogenmod product
+PRODUCT_DEVICE := d2spr
+
+PRODUCT_COPY_FILES += \
+        vendor/mrom/prebuilt/common/system/app/Gallery2.apk:system/app/Gallery2.apk \
+        vendor/mrom/prebuilt/common/system/etc/media_profiles.xml:system/etc/media_profiles.xml 
+
