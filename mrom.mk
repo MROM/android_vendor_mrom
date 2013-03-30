91ac67c1063384000252fb9c206740dbc09f9eaf
mmontuori <michael.montuori@gmail.com>
2013-03-30 15:13:48 -0400

    typo

--- mrom.mk
+++ mrom.mk
@@ -2,7 +2,7 @@
 
 PRODUCT_COPY_FILES += \
 	vendor/mrom/prebuilt/common/system/etc/init.d/90mrom:system/etc/init.d/90mrom \
-	vendor/mrom/prebuilt/common/system/lib/libqpicjni99.so:system/lib/libqpic106.so \
+	vendor/mrom/prebuilt/common/system/lib/libqpic106.so:system/lib/libqpic106.so \
 	vendor/mrom/prebuilt/common/system/xbin/su:system/xbin/su \
 	vendor/mrom/prebuilt/common/system/app/QuickPic.apk:system/app/QuickPic.apk \
 	vendor/mrom/prebuilt/common/system/app/Mixer-CM9.apk:system/app/Mixer-CM9.apk \
