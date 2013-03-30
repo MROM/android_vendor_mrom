f764f20bdadbcf8ec75bfc8486071550a0685e9f
mmontuori <michael.montuori@gmail.com>
2013-03-29 21:15:04 -0400

    removed Supersu, updated QuickPic, Cobalt Inferno, and Kernel 3.0.71

--- mrom.mk
+++ mrom.mk
@@ -3,9 +3,7 @@
 PRODUCT_COPY_FILES += \
 	vendor/mrom/prebuilt/common/system/etc/init.d/90mrom:system/etc/init.d/90mrom \
 	vendor/mrom/prebuilt/common/system/lib/libqpic106.so:system/lib/libqpic106.so \
-	vendor/mrom/prebuilt/common/system/xbin/su:system/xbin/su \
 	vendor/mrom/prebuilt/common/system/app/QuickPic.apk:system/app/QuickPic.apk \
-	vendor/mrom/prebuilt/common/system/app/Supersu.apk:system/app/Supersu.apk \
 	vendor/mrom/prebuilt/common/system/app/CobaltInferno.apk:system/app/CobaltInferno.apk \
 	vendor/mrom/CHANGELOG-MROM.txt:system/etc/CHANGELOG-MROM.txt
 
