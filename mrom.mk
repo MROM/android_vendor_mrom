e545f4fc93ae77ac46819bc92cba73e8a4e3017e
mmontuori <michael.montuori@gmail.com>
2013-04-06 12:34:39 -0400

    updated changelog and changed theme to CobaltCyanOrangen

--- mrom.mk
+++ mrom.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 	vendor/mrom/prebuilt/common/system/etc/init.d/90mrom:system/etc/init.d/90mrom \
 	vendor/mrom/prebuilt/common/system/lib/libqpic106.so:system/lib/libqpic106.so \
 	vendor/mrom/prebuilt/common/system/app/QuickPic.apk:system/app/QuickPic.apk \
-	vendor/mrom/prebuilt/common/system/app/CobaltInferno.apk:system/app/CobaltInferno.apk \
+	vendor/mrom/prebuilt/common/system/app/CobaltCyanOrangen.apk:system/app/CobaltCyanOrangen.apk \
 	vendor/mrom/CHANGELOG-MROM.txt:system/etc/CHANGELOG-MROM.txt
 
 $(call inherit-product, vendor/mrom/mrom-$(CM_BUILD).mk)
