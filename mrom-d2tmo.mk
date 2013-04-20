902596610e81de3196600af51f7a3fc3c55952f1
mmontuori <michael.montuori@gmail.com>
2013-04-19 21:02:28 -0400

    Updated changelog and removed Gallery2.apk prebuilt

--- mrom-d2tmo.mk
+++ mrom-d2tmo.mk
@@ -2,6 +2,5 @@
 PRODUCT_DEVICE := d2tmo
 
 PRODUCT_COPY_FILES += \
-        vendor/mrom/prebuilt/common/system/app/Gallery2.apk:system/app/Gallery2.apk \
         vendor/mrom/prebuilt/common/system/etc/media_profiles.xml:system/etc/media_profiles.xml 
 
