889ac5620f67800d608ac9a006cb3fed8c29c253
mmontuori <michael.montuori@gmail.com>
2013-05-24 17:28:08 -0400

    MROM encore

--- mrom-encore.mk
+++ mrom-encore.mk
@@ -1,3 +1,6 @@
 # Generic cyanogenmod product
 PRODUCT_DEVICE := encore
 
+PRODUCT_COPY_FILES += \
+        vendor/mrom/prebuilt/common/system/etc/media_profiles.xml:system/etc/media_profiles.xml 
+
